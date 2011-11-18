219da5eef1c283d8f944ca148cbd4c9eb440899c
Bernardo de Pádua <berpasan@gmail.com>
2011-11-17 23:17:41 -0200

    more 1.8.7

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-PontoRa::Application.config.session_store :cookie_store, key: '_ponto_ra_session'
+PontoRa::Application.config.session_store :cookie_store, :key => '_ponto_ra_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
