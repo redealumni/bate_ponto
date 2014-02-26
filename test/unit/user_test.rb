0bebda742e386ec12e35ee08e77457c79f46219b
danielgracia <daniel.santos@redealumni.com>
2014-02-26 17:46:31 -0300

    Fixing unit tests for User
    The hours_worked method in the User class doesn’t consider punch times
    in the future (which makes sense), but the tests didn’t account for
    that, so they were changed to use times from a day before, so they
    don’t randomly fail depending of the local time.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -8,68 +8,79 @@ class UserTest < ActiveSupport::TestCase
     @user = User.create(name: "Joao Cohones", password: "abc123")
   end
 
+  def yesterday_range
+    day = Date.yesterday
+    day.beginning_of_day..day.end_of_day
+  end
+
+  def yesterday_time(time)
+    Date.yesterday.beginning_of_day + time
+  end
+
+  private :yesterday_time, :yesterday_range
+
   test "hours_worked caso normal" do
     #entrou 1:00
-    @punches = @user.punches.create(punched_at: (Time.now.beginning_of_day + 1.hour))
+    @punches = @user.punches.create(punched_at: (yesterday_time(1.hour)))
     #saiu 5:00
-    @punches = @user.punches.create(punched_at: (Time.now.beginning_of_day + 5.hours))
+    @punches = @user.punches.create(punched_at: (yesterday_time(5.hours)))
     #entrou 10:00
-    @punches = @user.punches.create(punched_at: (Time.now.beginning_of_day + 10.hours))
+    @punches = @user.punches.create(punched_at: (yesterday_time(10.hours)))
     #saiu 15:00
-    @punches = @user.punches.create(punched_at: (Time.now.beginning_of_day + 15.hours))
+    @punches = @user.punches.create(punched_at: (yesterday_time(15.hours)))
 
-    assert_equal 9, @user.hours_worked(Time.now.beginning_of_day..Time.now.end_of_day)
+    assert_equal 9, @user.hours_worked(yesterday_range)
   end
 
   test "hours_worked punches falta fim" do
     #entrou 1:00
-    @punches = @user.punches.create(punched_at: (Time.now.beginning_of_day + 1.hour))
+    @punches = @user.punches.create(punched_at: (yesterday_time(1.hour)))
     #saiu 5:00
-    @punches = @user.punches.create(punched_at: (Time.now.beginning_of_day + 5.hours))
+    @punches = @user.punches.create(punched_at: (yesterday_time(5.hours)))
     #entrou 10:00
-    @punches = @user.punches.create(punched_at: (Time.now.beginning_of_day + 10.hours))
+    @punches = @user.punches.create(punched_at: (yesterday_time(10.hours)))
 
     #18 horas, até o fim do período
-    assert_equal 18, @user.hours_worked(Time.now.beginning_of_day..Time.now.end_of_day)
+    assert_equal 18, @user.hours_worked(yesterday_range)
   end
 
   test "hours_worked punches falta início" do
     #saiu 5:00
-    @punches = @user.punches.create(punched_at: (Time.now.beginning_of_day + 5.hours))
+    @punches = @user.punches.create(punched_at: (yesterday_time(5.hours)))
     #entrou 10:00
-    @punches = @user.punches.create(punched_at: (Time.now.beginning_of_day + 10.hours))
+    @punches = @user.punches.create(punched_at: (yesterday_time(10.hours)))
     #saiu 17:00
-    @punches = @user.punches.create(punched_at: (Time.now.beginning_of_day + 17.hours))
+    @punches = @user.punches.create(punched_at: (yesterday_time(17.hours)))
 
     #12 horas, desde o início do dia
-    assert_equal 12, @user.hours_worked(Time.now.beginning_of_day..Time.now.end_of_day)
+    assert_equal 12, @user.hours_worked(yesterday_range)
   end
 
 
   test "hours_worked ignora entrada faltante no meio" do
-    @punches = @user.punches.create(punched_at: (Time.now.beginning_of_day + 1.hours)) #entra
+    @punches = @user.punches.create(punched_at: (yesterday_time(1.hours))) #entra
                                                                               # 3:00       sai
-    @punches = @user.punches.create(punched_at: (Time.now.beginning_of_day + 5.hours)) #sai
-    @punches = @user.punches.create(punched_at: (Time.now.beginning_of_day + 10.hours)) #entra
-    @punches = @user.punches.create(punched_at: (Time.now.beginning_of_day + 15.hours)) #sai
+    @punches = @user.punches.create(punched_at: (yesterday_time(5.hours))) #sai
+    @punches = @user.punches.create(punched_at: (yesterday_time(10.hours))) #entra
+    @punches = @user.punches.create(punched_at: (yesterday_time(15.hours))) #sai
 
-    @punches = @user.punches.create(punched_at: (Time.now.beginning_of_day + 20.hours))
-    @punches.update_attribute(:punched_at, (Time.now.beginning_of_day + 3.hours) ) #  sai lá atrás
+    @punches = @user.punches.create(punched_at: (yesterday_time(20.hours)))
+    @punches.update_attribute(:punched_at, (yesterday_time(3.hours)) ) #  sai lá atrás
 
-    assert_equal 7, @user.hours_worked(Time.now.beginning_of_day..Time.now.end_of_day)
+    assert_equal 7, @user.hours_worked(yesterday_range)
   end
 
   test "hours_worked ignora saída faltante no meio" do
-    @punches = @user.punches.create(punched_at: (Time.now.beginning_of_day + 1.hours)) #entra
-    @punches = @user.punches.create(punched_at: (Time.now.beginning_of_day + 5.hours)) #sai
+    @punches = @user.punches.create(punched_at: (yesterday_time(1.hours))) #entra
+    @punches = @user.punches.create(punched_at: (yesterday_time(5.hours))) #sai
                                                                               # 8         entra
-    @punches = @user.punches.create(punched_at: (Time.now.beginning_of_day + 10.hours))#entra
-    @punches = @user.punches.create(punched_at: (Time.now.beginning_of_day + 15.hours))#sai
+    @punches = @user.punches.create(punched_at: (yesterday_time(10.hours)))#entra
+    @punches = @user.punches.create(punched_at: (yesterday_time(15.hours)))#sai
 
-    @punches = @user.punches.create(punched_at: (Time.now.beginning_of_day + 20.hours))#some
-    @punches.update_attribute(:punched_at, (Time.now.beginning_of_day + 8.hours) ) # entra lá atrás
+    @punches = @user.punches.create(punched_at: (yesterday_time(20.hours)))#some
+    @punches.update_attribute(:punched_at, (yesterday_time(8.hours)) ) # entra lá atrás
     
-    assert_equal 11, @user.hours_worked(Time.now.beginning_of_day..Time.now.end_of_day)
+    assert_equal 11, @user.hours_worked(yesterday_range)
   end
 
 end
