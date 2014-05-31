f362cd07037c6dac8bf4b1e860f09166d6e1fc3e
Thiago Brandão <thiagobrandam@gmail.com>
2014-05-31 16:51:10 -0300

    Adds punch creation action to api

--- app/controllers/api/punches_controller.rb
+++ app/controllers/api/punches_controller.rb
@@ -5,7 +5,25 @@ module Api
     respond_to :json
 
     def index
-      respond_with @user.punches
+      respond_with @user.punches.latest.first(10)
+    end
+
+    def create
+      last_punch = @user.punches.latest.first
+
+      if last_punch.present? && last_punch.created_at > 5.minutes.ago
+        #remove punches sequenciais (para correção rápida)
+        removed_punch = last_punch.destroy
+        respond_with removed_punch, status: :ok
+      else
+        @punch = @user.punches.new(create_params)
+
+        if @punch.save
+          respond_with @punch, status: :created
+        else
+          respond_with @punch.errors, status: :unprocessable_entity
+        end
+      end
     end
 
     protected
@@ -16,6 +34,11 @@ module Api
       if @user.blank? || params[:api_token] != API_TOKEN
         head :unauthorized
       end
+    end
+
+    # Get params for creating
+    def create_params
+      params.require(:punch).permit(:comment)
     end
   end
 end
