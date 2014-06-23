245470085c5a8099e61c03b344909e1624e4f0ab
Thiago Brandão <thiagobrandam@gmail.com>
2014-06-23 11:34:47 -0300

    Loads fixtures automatically

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,18 +2,8 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
-# Load Turn
-begin
-  require 'turn/autorun'
-rescue LoadError 
-end
-
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
-  # fixtures :all
+  ActiveRecord::Migration.check_pending!
 
-  # Add more helper methods to be used by all tests here...
+  fixtures :all
 end
