5047618eb2d3b8e84f27d3a1af62bfa14f4ed7e3
Thiago Brandão <thiagobrandam@gmail.com>
2014-06-27 21:27:01 -0300

    Comments out hook that doesn’t work anymore

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,7 +1,7 @@
-current_path = "/data/#{config.app}/current"
-environment  = config.node[:environment][:framework_env]
+# current_path = "/data/#{config.app}/current"
+# environment  = config.node[:environment][:framework_env]
 
-# cron
-if config.node[:instance_role] == 'solo' || config.node[:instance_role] == 'app_master'
-  run "cd #{config.release_path} && bundle exec whenever --update-crontab '#{config.app}' --set 'rails_root=#{current_path}&environment=#{environment}'"
-end
+# # cron
+# if config.node[:instance_role] == 'solo' || config.node[:instance_role] == 'app_master'
+#   run "cd #{config.release_path} && bundle exec whenever --update-crontab '#{config.app}' --set 'rails_root=#{current_path}&environment=#{environment}'"
+# end
