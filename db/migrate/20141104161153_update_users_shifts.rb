971d1b0d629a7e1aca8ba4866bb497278715d36e
victorpre <victor.presumido@gmail.com>
2014-11-05 10:54:41 -0200

    Updates migration

--- db/migrate/20141104161153_update_users_shifts.rb
+++ db/migrate/20141104161153_update_users_shifts.rb
@@ -2,7 +2,7 @@ class UpdateUsersShifts < ActiveRecord::Migration
   def change
     User.find_each do |user|
       user.shifts = Shifts.new_default
-      user.save
+      user.save!
     end
   end
 end
