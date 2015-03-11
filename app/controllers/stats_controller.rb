983d183598c02581b6df8135d247455729aa5358
João Vitor <joao.vitor@redealumni.com>
2015-03-11 17:53:19 -0300

    more readable

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -123,10 +123,13 @@ class StatsController < ApplicationController
 
   def reports_punch
     begin_split = params[:start_date].split("\/")
-    end_split = params[:end_date].split("\/")
     begin_date  = Time.zone.local(begin_split[2],begin_split[1],begin_split[0])
-    end_date  = Time.zone.local(end_split[2],end_split[1],end_split[0])
-    punches = Punch.where(created_at: begin_date .. end_date)
+
+    end_split   = params[:end_date].split("\/")
+    end_date    = Time.zone.local(end_split[2],end_split[1],end_split[0])
+
+    punches     = Punch.where(created_at: begin_date .. end_date)
+
     data = punches.collect do |punche|
       {
         criado_em: punche.created_at.strftime("%d/%m/%y %I:%M%p"),
